892c72146f1a28cde1664d76e912dcb908fb8f0e
shahnawaz-khan-pathan <39399094+shahnawaz-khan-pathan@users.noreply.github.com>
2021-06-08 13:16:23 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ class Hello {
     public static void main(String[] args) {
         int a =5;
         int b = 6;
-        int sum = x + y;        
+        int sum = a + b;        
         System.out.println("Hello Shahnawaz!",sum); 
     }
 }
