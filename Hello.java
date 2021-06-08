425bab3f25a924589859bec1124218c535729700
shahnawaz-khan-pathan <39399094+shahnawaz-khan-pathan@users.noreply.github.com>
2021-06-08 13:15:55 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 // Your First Program
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Shahnawaz!"); 
+        int a =5;
+        int b = 6;
+        int sum = x + y;        
+        System.out.println("Hello Shahnawaz!",sum); 
     }
 }
